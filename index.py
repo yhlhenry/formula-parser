1982beca252731fdcbe0095b2c6b86f4aa782944
FIRST_NAME LAST_NAME <FIRST_NAME LAST_NAME>
2021-10-20 11:24:35 +0800

    add parse route

--- index.py
+++ index.py
@@ -13,10 +13,10 @@ def select_records():
     if request.method == 'POST':
         # 偷看一下 request.form 
         print(request.form)
-        python_records = web_select_specific(request.form)
-        return render_template("show_records.html", html_records=python_records)
-    else:
-        return render_template("select_records.html")
+        parsedFormula = 'web_select_specific(request.form)'
+        return render_template("index.html", parsedFormula=parsedFormula)
+    # else:
+    #     return render_template("select_records.html")
 
 if __name__ == "__main__":
 
