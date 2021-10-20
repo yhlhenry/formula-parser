e5ae66e06ccd1a28e8e6713441179373c93dcf9f
FIRST_NAME LAST_NAME <FIRST_NAME LAST_NAME>
2021-10-20 10:52:17 +0800

    add parse route

--- index.py
+++ index.py
@@ -8,6 +8,15 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/parse", methods=['GET', 'POST'])
+def select_records():
+    if request.method == 'POST':
+        # 偷看一下 request.form 
+        print(request.form)
+        python_records = web_select_specific(request.form)
+        return render_template("show_records.html", html_records=python_records)
+    else:
+        return render_template("select_records.html")
 
 if __name__ == "__main__":
 
